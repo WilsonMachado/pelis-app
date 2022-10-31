d64d90a717d89c0cca1633388fcfd9615e2adb53
WilsonMachado <wilson.machado@correounivalle.edu.co>
2022-10-30 23:34:42 -0500

    Se añade el Card Swiper desde un widget de terceros para crear el carrusel de películas en cartelera. El recurso dado por Fernando no funcionó bien, usé el recurso de la app pasada (la app de widgets) para que me funcionara

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -5,10 +6,31 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    ///* El MediaQuery es un widget que facilita la obtención de la información relacionada a la plataforma en la que está corriendo la app, las dimensiones del dispositivo, la orientación, entre otras cosas.
+    
+    final size = MediaQuery.of(context).size;
+
     return Container(
       width: double.infinity,
-      height: 500,
+      height: size.height * 0.5,
       color: Colors.red,
+      child: Swiper(
+        
+        itemCount: 10, // Cantidad de tarjetas que vamos a poner
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.9,
+        
+        ///* Función que se dispara construir el widget. Si no se usa el BuildContext, se pone un guión al piso: ( _, int index) por ejemplo. Lo importante de esta función es devolver un widget para renderizar la tarjeta.
+
+        itemBuilder: ( _, int index) { 
+          return const FadeInImage(
+            placeholder: NetworkImage('https://picsum.photos/500/300?image=1'), // Imagen de loading
+            image: NetworkImage('https://picsum.photos/500/300?image=1') // Imagen que viene de internet
+            );
+        }, 
+        ),
     );
   }
 }
